3b07f1f95c13bf4b632aba44e91fce1a4e47ab64
afruz <hubozbek@gmail.com>
2026-06-17 17:43:15 +0500

    Refactor STATIC_ROOT path definition

--- ayva2/settings.py
+++ ayva2/settings.py
@@ -68,7 +68,8 @@ TIME_ZONE = 'Asia/Tashkent'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
